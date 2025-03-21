38d73c7307f6a441c4a44ccbb29a42576169b707
Monika <monikazarudzka@gmail.com>
2025-03-21 22:05:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import heart from './heart.png';
 import './App.css';
+import { useState, useEffect , Button, Link} from 'react';
 
+
+let heartSize;
 function App() {
+
+  const [heartSize, setHeartSize] = useState(50);
+  const [ifGrows, setIfGrows] = useState(1);
+  let [timeInterval, setTimeInterval] = useState(0);
+
+
+  function handleClick(){
+    <Link to="/another-page">
+      <button>Go to Another Page</button>
+    </Link>
+  }
+
+  useEffect(() =>{
+    const interval = setInterval(() => {
+      if(heartSize == 50)  
+        setHeartSize(65);
+      else if(heartSize==65)
+        setHeartSize(50);
+
+      setTimeInterval(Math.max(-400, timeInterval-6));
+     
+    }, 450 + timeInterval);
+
+    return () => clearInterval(interval);
+  }, [heartSize, timeInterval]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div styles ="height: 50%">
+          <header className="App-header">
+            <a href="gallery.html" onClick={handleClick}>
+            <img src={heart} className="heart-logo"  style={{width: `${heartSize}%`}} alt="logo" />
+            </a>
+         
+          <p>
+          My heart beats this fast when I see you! <br></br>
+          Click the heart!!!
+          </p>
+   
+        </header>
+      </div>
     </div>
   );
 }
